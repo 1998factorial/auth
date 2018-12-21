0d73ce9a6e92475902c6417e968e69dfacc7525d
1998factorial <edward19980505@gmail.com>
2018-12-21 11:06:23 +0800

    added code for changing samba password

--- route.py
+++ route.py
@@ -11,10 +11,11 @@ from datetime import datetime
 from User import * #need to implement user class
 from functools import wraps
 import os,sys,ldap
+import subprocess
 
 
 Server = "ldap://192.168.1.115:389" 
-
+is_admin = False
 
 def login_required(f): #user need to log in first in order to access home page
     @wraps(f)
@@ -25,8 +26,8 @@ def login_required(f): #user need to log in first in order to access home page
             flash("YOU NEED TO LOG IN FIRST")
             return redirect(url_for('login'))
     return wrap
-
-def change_password(username,old_password,new_password):
+"""
+def change_password_as_admin(username,old_password,new_password):
     baseDN = "dc=example,dc=com"
     searchScope = ldap.SCOPE_SUBTREE
     if username != "admin":
@@ -41,8 +42,47 @@ def change_password(username,old_password,new_password):
         return False
 
     try:
-        adpw = new_password.encode("utf-8")
+        adpw = new_password.encode("utf-8") #有待商榷
+        c.simple_bind_s("cn=admin,dc=example,dc=com","test123456")
         c.modify_s(username,[(ldap.MOD_REPLACE, "userPassword", adpw)])
+        c.modify_s(username,[(ldap.MOD_REPLACE, "sambaLMPassword", adpw)])
+        c.modify_s(username,[(ldap.MOD_REPLACE, "sambaNTPassword", adpw)])
+        flash("CHANGED PASSWORD")
+        return True
+    except ldap.LDAPError as error_message:
+        flash("CANNOT CHANGE PASSWORD")
+        print(error_message)
+        return False
+"""
+
+def change_password(username,old_password,new_password):
+    baseDN = "dc=example,dc=com"
+    searchScope = ldap.SCOPE_SUBTREE
+    if session['username'] != username and username != 'admin':
+        return False
+    if is_admin:
+        print("change password as admin")
+    if username != "admin":
+        username = "uid=" + username + ",ou=people," + baseDN #expected form
+    else:
+        username = "cn=admin,dc=example,dc=com"
+    c = ldap.initialize(Server)
+    try:
+        c.simple_bind_s(username,old_password)
+    except ldap.LDAPError as e:
+        flash("invalid credentials")
+        return False
+    try:
+        adpw = new_password.encode("utf-8")
+        out1 = subprocess.run(['./mkntpwd','-N',new_password],stdout=subprocess.PIPE)
+        out2 = subprocess.run(['./mkntpwd','-L',new_password],stdout=subprocess.PIPE)
+        pwd_N = out1.stdout
+        pwd_L = out2.stdout
+        print("____________")
+        c.modify_s(username,[(ldap.MOD_REPLACE, "userPassword", adpw)])
+        c.simple_bind_s("cn=admin,dc=example,dc=com","test123456")#bind admin
+        c.modify_s(username,[(ldap.MOD_REPLACE, "sambaLMPassword", pwd_L[0:len(pwd_L)-1])])
+        c.modify_s(username,[(ldap.MOD_REPLACE, "sambaNTPassword", pwd_N[0:len(pwd_N)-1])])
         flash("CHANGED PASSWORD")
         return True
     except ldap.LDAPError as error_message:
@@ -54,9 +94,11 @@ def change_password(username,old_password,new_password):
 def check_password(username,password): #verify
     baseDN = "dc=example,dc=com"
     searchScope = ldap.SCOPE_SUBTREE
+    log_in_as_admin = False
     if username != "admin":
         username = "uid=" + username + ",ou=people," + baseDN #expected form
     else:
+        log_in_as_admin = True
         username = "cn=admin,dc=example,dc=com"
     #username = "cn=admin,dc=example,dc=com"
     searchFilter = "sAMAccountName" + username
@@ -66,6 +108,10 @@ def check_password(username,password): #verify
         c = ldap.initialize(Server)
         c.simple_bind_s(username,password)
         print("IS VALID!")
+        if log_in_as_admin:
+            session['is_admin'] = True
+        else:
+            session['is_admin'] = False
         return True
     except ldap.LDAPError as error_message:
         flash("Invalid credentials\n")
@@ -97,6 +143,7 @@ def change_password_route():
     return render_template('change_password.html')
 
 
+
 @app.route('/login', methods=['POST','GET'])
 def login():
     if request.method == 'POST':
@@ -105,6 +152,10 @@ def login():
         password = request.form['password']
         if check_password(username,password): #verify succeed
             session['logged_in'] = True #let user login
+            session['username'] = username
+            session['password'] = password
+            if session['is_admin']:
+                print("log in as admin")
             #session['userID'] = User.find_user_id(username,password) #set user id
             return redirect(url_for('index')) #redirect to index page
 
@@ -136,8 +187,9 @@ def logout():
         session.pop('password')
     if session.get('logged_in'):
         session.pop('logged_in')
+    session['is_admin'] = False
     return redirect(url_for('login')) #redirect user to login page
-
+"""
 	if username and password:
 	    is_login = True
 	    # please add login code here.  and please set is_login = true if succeeded. 
@@ -153,3 +205,4 @@ def logout():
 		    return  Response(fail_json("CANNOT Login. something error!"), mimetype='application/json')
 	else
         return  Response(fail_json("username or password not found!"), mimetype='application/json')
+"""
