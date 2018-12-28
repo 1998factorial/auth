98713033429c27fd24604b306c17876c07e552ea
1998factorial <edward19980505@gmail.com>
2018-12-28 17:10:37 +0800

    added snap

--- snap/index.js
+++ snap/index.js
@@ -1,71 +1,76 @@
-'use strict';
-
-var os = require('os');
-var nodeStatic = require('node-static');
-var http = require('http');
-var socketIO = require('socket.io');
-
-var fileServer = new(nodeStatic.Server)();
-var app = http.createServer(function(req, res) {
-  fileServer.serve(req, res);
-}).listen(8080);
-
-var io = socketIO.listen(app);
-io.sockets.on('connection', function(socket) {
-
-  // convenience function to log server messages on the client
-  function log() {
-    var array = ['Message from server:'];
-    array.push.apply(array, arguments);
-    socket.emit('log', array);
-  }
-
-  socket.on('message', function(message) {
-    log('Client said: ', message);
-    // for a real app, would be room-only (not broadcast)
-    socket.broadcast.emit('message', message);
-  });
-
-  socket.on('create or join', function(room) {
-    log('Received request to create or join room ' + room);
-
-    var clientsInRoom = io.sockets.adapter.rooms[room];
-    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-    log('Room ' + room + ' now has ' + numClients + ' client(s)');
-
-    if (numClients === 0) {
-      socket.join(room);
-      log('Client ID ' + socket.id + ' created room ' + room);
-      socket.emit('created', room, socket.id);
-    } else if (numClients === 1) {
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      // io.sockets.in(room).emit('join', room);
-      socket.join(room);
-      socket.emit('joined', room, socket.id);
-      io.sockets.in(room).emit('ready', room);
-      socket.broadcast.emit('ready', room);
-    } else { // max two clients
-      socket.emit('full', room);
+window.onload=function(){
+    document.getElementById("id-face").addEventListener("change", function(){       
+        onFileChange(this,"face-result","face-empty-result")
+    });
+    document.getElementById("id-back").addEventListener("change", function(){       
+        onFileChange(this,"back-result","back-empty-result")
+    });
+    document.getElementsByClassName("btn")[0].addEventListener("click", function(){       
+        submit();
+    });
+};
+/**
+ * 选中图片时的处理
+ * @param {*} fileObj input file元素
+ * @param {*} el //选中后用于显示图片的元素ID
+ * @param {*} btnel //未选中图片时显示的按钮区域ID
+ */
+function onFileChange(fileObj,el,btnel){
+    var windowURL = window.URL || window.webkitURL;
+    var dataURL;
+    var imgObj = document.getElementById(el);
+    document.getElementById(btnel).style.display="none";
+    imgObj.style.display="block";
+    if (fileObj && fileObj.files && fileObj.files[0]) {
+        dataURL = windowURL.createObjectURL(fileObj.files[0]);
+        imgObj.src=dataURL;
+    } else {
+        dataURL = fileObj.value;
+        imgObj.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)";
+        imgObj.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = dataURL;
     }
-  });
-
-  socket.on('ipaddr', function() {
-    var ifaces = os.networkInterfaces();
-    for (var dev in ifaces) {
-      ifaces[dev].forEach(function(details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-          socket.emit('ipaddr', details.address);
+}
+/**
+ * 将图片压缩后返回base64格式的数据
+ * @param {*} image img元素
+ * @param {*} width 压缩后图片宽度
+ * @param {*} height 压缩后图片高度
+ * @param {*} qua //图片质量1-100
+ */
+function compressImageTobase64(image,width,height,qua){
+    var quality = qua ? qua / 100 : 0.8;
+    var canvas = document.createElement("canvas"),     
+        ctx = canvas.getContext('2d');     
+    var w = image.naturalWidth,     
+        h = image.naturalHeight;     
+    canvas.width = width||w;     
+    canvas.height = height||h;     
+    ctx.drawImage(image, 0, 0, w, h, 0, 0, width||w, height||h);
+    var data = canvas.toDataURL("image/jpeg", quality);     
+    return data;
+}
+//提交
+function submit(){
+    //1、form提交
+    //document.getElementById("mainForm").submit();
+    //2、压缩后ajax提交
+    var face_data=compressImageTobase64(document.getElementById("face-result"),200,100,90);
+    var back_data=compressImageTobase64(document.getElementById("back-result"),200,100,90);
+    var formData = new FormData();  
+    formData.append("face",face_data);
+    formData.append("back",back_data);
+    //需引入jQuery
+    $.ajax({
+        url:"/地址",
+        type: 'POST',
+        cache: false,
+        data: formData,
+        timeout:180000,
+        processData: false,
+        contentType: false,
+        success:function(r){
+        },
+        error:function(r){  
         }
-      });
-    }
-  });
-
-  socket.on('disconnect', function(reason) {
-    console.log(`Peer or server disconnected. Reason: ${reason}.`);
-    socket.broadcast.emit('bye');
-  });
-
-  socket.on('bye', function(room) {
-    console.log(`Peer said bye on room ${room}.`);
-  });
-});
+   });
+}
